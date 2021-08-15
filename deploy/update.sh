80fe2ad2ffd8571b27830856aa8e481756663f3f
Marcel Irawan <theofilusmarcel1@gmail.com>
2021-08-15 20:30:51 +0700

    change update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,7 @@ PROJECT_BASE_PATH='/usr/local/apps/wishlist-api'
 git pull
 pipenv lock -r > $PROJECT_BASE_PATH/requirements.txt
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/python manage.py makemigrations
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart wishlist_api
